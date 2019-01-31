73ac185b6aa75f60be83b335d0a8497f7e5862d1
Ryan Macdonald <ryan.macdonald15@stjohns.edu>
2019-01-30 21:18:59 -0500

    Part A Playground

---
+++ helper_utils.py
@@ -0,0 +1,9 @@
+#Define a
+def is_even(x):
+    if (x % 2) == 0:
+        return True
+    else:
+        return False
+
+def square(x):
+    return x*x
---
+++ playground.py
@@ -0,0 +1,7 @@
+from helper_utils import square
+
+print("Modules")
+print("x = 100, x*x = ")
+print(square(100))
+
+print("End Modules")
